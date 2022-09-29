10562adc7fbbf33e162b0da010c2fbf6003ea591
b-sandrine <sanrdushimimana@gmail.com>
2022-09-29 19:18:39 +0200

    Basic things to know about kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+//    println("Hello World!")
+    val myString = "Sandrine is funny"
+    println("The sentence I wrote is $myString")
+    println(myString.length)
+    println(myString.substring(2,4))
+    println(myString[0])
+}
